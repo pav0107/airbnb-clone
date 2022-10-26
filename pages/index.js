5a6e7d14d9f32794f7b7f5811a9e63717dc77cbf
Pav <pav0107@gmail.com>
2022-10-26 21:28:27 +0100

    add medium cards

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
+import MediumCard from '../components/MediumCard';
 import SmallCard from '../components/SmallCard';
 
-export default function Home({ exploreData }) {
+export default function Home({ exploreData, cardsData }) {
   return (
     <div className="">
       <Head>
@@ -21,13 +22,22 @@ export default function Home({ exploreData }) {
 
           {/* Pull data from server */}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {exploreData.map(({ img, distance, location }) => (
+            {exploreData?.map(({ img, distance, location }) => (
               <SmallCard
                 key={img}
                 img={img}
                 distance={distance}
                 location={location}
               />
+            ))}
+          </div>
+        </section>
+
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardsData?.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
             ))}
           </div>
         </section>
@@ -41,9 +51,14 @@ export async function getStaticProps() {
     (res) => res.json()
   );
 
+  const cardsData = await fetch('https://www.jsonkeeper.com/b/VHHT').then(
+    (res) => res.json()
+  );
+
   return {
     props: {
       exploreData,
+      cardsData,
     },
   };
 }
