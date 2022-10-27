ac4bbf301ef7bd08dd618da96928a3f8a5551eca
Pav <pav0107@gmail.com>
2022-10-27 09:52:55 +0100

    add footer

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>dolor sit</p>
+        <p>amet consectetur</p>
+        <p>adipisicing elit</p>
+        <p>consequuntur pariatur</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>dolorem eos</p>
+        <p>odit doloribus</p>
+        <p>Quam id</p>
+        <p>non eos</p>
+        <p>voluptatibus suscipit</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>dolorum repudiandae</p>
+        <p>repellat nobis</p>
+        <p>quod obcaecati</p>
+        <p>vel mollitia</p>
+        <p>repellendus asperiores</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
