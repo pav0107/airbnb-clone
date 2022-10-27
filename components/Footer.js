d335914ee1ac34b905ee81c4f815550b2bea8f15
Pav <pav0107@gmail.com>
2022-10-27 11:55:46 +0100

    change footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,22 +3,31 @@ function Footer() {
     <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">ABOUT</h5>
-        <p>How Airbnb works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
+        <p>amet quod</p>
+        <p>praesentium libero</p>
+        <p>Perferendis odit</p>
+        <p>expedita at</p>
+        <p>repudiandae cumque</p>
       </div>
-
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora et animi
+      doloribus, cumque illum nostrum beatae fuga consequuntur dolorem,
+      asperiores consectetur eaque debitis, mollitia optio autem corporis quod
+      nulla eligendi perferendis cupiditate quam provident. Ut cupiditate quam
+      tempore hic modi laboriosam ipsa dolor. Magnam ullam id officia omnis eos
+      consequuntur aspernatur ducimus eaque repellat, amet quod rerum iusto
+      dolorem molestiae, cupiditate dolores sequi eligendi facilis quibusdam
+      quia praesentium libero, neque soluta? Perferendis odit consequuntur nulla
+      voluptates fugit dicta excepturi. Vel laborum fugiat expedita at omnis et
+      repellendus perferendis repudiandae cumque doloribus consectetur culpa
+      ullam est harum, saepe, illum sequi inventore.
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">COMMUNITY</h5>
-        <p>Accessibility</p>
+        <p>sequi inventore</p>
         <p>dolor sit</p>
         <p>amet consectetur</p>
         <p>adipisicing elit</p>
         <p>consequuntur pariatur</p>
       </div>
-
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">HOST</h5>
         <p>dolorem eos</p>
@@ -27,7 +36,6 @@ function Footer() {
         <p>non eos</p>
         <p>voluptatibus suscipit</p>
       </div>
-
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">SUPPORT</h5>
         <p>dolorum repudiandae</p>
